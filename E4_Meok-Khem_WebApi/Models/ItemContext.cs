e5ab74fee714f9d66ccaa80ee0b5a874cde41cf2
Meok Khem <113187567+KhemMeok@users.noreply.github.com>
2023-02-26 14:19:14 +0700

    first commit

---
+++ E4_Meok-Khem_WebApi/Models/ItemContext.cs
@@ -0,0 +1,13 @@
+﻿ using Microsoft.EntityFrameworkCore;
+namespace E4_Meok_Khem_WebApi.Models
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ItemModel> TodoItems { get; set; } = null!;
+    }
+}
---
+++ E4_Meok-Khem_WebApi/Models/ItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace E4_Meok_Khem_WebApi.Models
+{
+    public class ItemModel
+    {   
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public Boolean Status { get; set; }
+    }
+}
